f5bf6c40f048276e308aa12b64b1b361af7bd0a3
Savannah Cortes <savannahabrantley@gmail.com>
2018-05-26 14:04:16 -0500

    basic structure for quiz app

---
+++ scripts/index.js
@@ -0,0 +1,13 @@
+function resetQuiz() {
+
+
+}
+
+$(document).ready(function() {
+    // Start timer on button click 
+    /*
+    $("#StartCountdownBtn").on("click", function () { 
+        StartCountdownTimer(5, 'm', timerCallback);
+    })
+    */
+})
