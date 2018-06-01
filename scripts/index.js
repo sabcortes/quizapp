ad9f7852eb7d855510392bc4204c6e7021d64f43
Savannah Cortes <savannahabrantley@gmail.com>
2018-06-01 15:41:43 -0500

    commit before peer feedback

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,136 @@
-function resetQuiz() {
-
-
-}
+let questionNumber = 0;
+let score= 0;
 
 $(document).ready(function() {
-    // Start timer on button click 
-    /*
-    $("#StartCountdownBtn").on("click", function () { 
-        StartCountdownTimer(5, 'm', timerCallback);
-    })
-    */
-})
+  $('.startPage').show();
+
+  $('#quizStart').on('click', event => {
+    $('.startPage').hide();
+    $('#js-questionsAnswers').show(startQuiz());
+    $('.questionNumber').text(1);
+  });
+
+  $('#nextQuestion').on('click', event => {  
+      event.preventDefault();
+      userSelectedAnswerChoice();   
+  });
+
+  $('#continue').on('click', event => {
+    event.preventDefault();
+    continueFromFeedbackToNextQuestion();
+  });
+
+  $('#restart').on('click', event=> {
+    restartQuiz();
+  });
+  
+  console.log("click to start works");
+});
+
+function displayQuestions() {
+  //for (i=0; i <STORE.length; i ++) {
+    if (questionNumber < STORE.length) {
+      $('#question').html(`${STORE[questionNumber].question}`);
+      $('#js-questionsAnswers  > fieldset').html(`<div class="box">
+        <input type= "radio" name="answerOption" value = "${STORE[questionNumber].answers[0]}" required></input>
+        <label class= "answerChoice">
+        <span>${STORE[questionNumber].answers[0]}</span>
+        </label>
+        </div>
+        <div class="box">
+        <input type= "radio" name="answerOption" value = "${STORE[questionNumber].answers[1]}" required></input>
+        <label class= "answerChoice">
+        <span>${STORE[questionNumber].answers[1]}</span>
+        </label>
+        </div>
+        <div class= "box">
+        <input type= "radio" name="answerOption" value = "${STORE[questionNumber].answers[2]}" required></input>
+        <label class= "answerChoice">
+        <span>${STORE[questionNumber].answers[2]}</span>
+        </label>
+        </div>
+        <div class="box">
+        <input type= "radio" name="answerOption" value = "${STORE[questionNumber].answers[3]}" required></input>
+        <label class= "answerChoice">
+        <span>${STORE[questionNumber].answers[3]}</span>
+        </label>
+        </div>`
+        );
+    }
+    else {
+      $('.js-quiz').hide();
+      $('.js-feedback').show();
+      renderResults();
+      restartQuiz();
+    };
+  };
+//};
+
+function renderQuestion() {
+  $('#js-questionsAnswers').html(displayQuestions());
+  console.log("display questions works");
+};
+
+function userSelectedAnswerChoice() {
+    let selected = $('input:checked');
+    let answer= selected.val();
+    let correctAnswer = `${STORE[questionNumber].answer}`;
+    $('#quiz').attr("hidden", true);
+
+    if(answer===correctAnswer) {
+      $('#feedbackAnswer').removeClass("incorrect").addClass("correct");
+      $('#feedbackAnswer > h2').html("Whoop!");
+      $('#feedbackAnswer > span').html(`${STORE[questionNumber].feedback}`);
+      updateScore();
+    }
+    else{
+      $('#feedbackAnswer').removeClass("correct").addClass("incorrect");
+      $('#feedbackAnswer > h2').html("Uh-oh!");
+      $('#feedbackAnswer > span').html(`The correct answer is ${STORE[questionNumber].answer} <div>${STORE[questionNumber].feedback}</div>`);
+    };
+    
+    $('#feedbackSection').attr("hidden", false);
+
+     console.log("click event is successful");
+};
+
+function updateScore(){
+  score ++;
+  $('.score').text(score);
+};
+
+function changeQuestionNumber(){
+  questionNumber ++;
+  $('.questionNumber').text(questionNumber +1);
+};
+
+function continueFromFeedbackToNextQuestion(){
+  changeQuestionNumber();
+  renderQuestion();
+  $('#quiz').attr("hidden", false);
+  $('#feedbackSection').attr("hidden", true);
+};
+
+function startQuiz() {
+  renderQuestion();
+  $('#quiz').attr("hidden", false);
+};
+
+function renderResults(){
+  if (score > 6) {
+    $('.js-feedback').html(`
+    <div class= "results"><h3>Gig 'em, Aggie! You got ${score} out of 10 right!</h3>
+    <button id= "restart">Restart Quiz!</button>
+    </div>`);
+  }
+  else {
+    $('.js-feedback').html(`<div class="results"><h3>Oh no! You must not be a former student.</h3>
+    <span>You got ${score} out of 10 right. Want to try again?</span>
+    <button id="restart">Restart Quiz!</button>
+    </div>`);
+  };
+};
+
+function restartQuiz () {
+  startQuiz();
+};
---
+++ scripts/questions.js
@@ -0,0 +1,122 @@
+const STORE= [
+    {
+       question: "In what year did Texas A&#38;M University open their doors?",
+       answers: [
+           "1870",
+           "1876",
+           "1889",
+           "1902",
+       ],
+       answer: "1876",
+       feedback: "The University was established by the Morrill Act, a land-grant, that provided public lands for the purpose of higher education. Texas A&#38;M is the first land-grant university in the state of Texas.",
+   },
+
+   {
+       question: "What was Texas A&#38;M first called?",
+       answers: [
+           "Texas Agricultural and Mechanical University",
+           "Agricultural and Mechanical College of Texas",
+           "Texas University",
+           "Texas A&#38;M University",
+       ],
+       answer: "Agricultural and Mechanical College of Texas",
+       feedback: "The school received University status in 1963 and changed its name from the Agricultural and Mechanical College of Texas to Texas A&#38;M University.",
+   },
+
+   {
+       question: "1963 in an important year in the history of Texas A&#38;M University, why?",
+       answers: [
+           "The University is renamed",
+           "African-Americans are first allowed admittance into the University",
+           "Women are first allowed admittance into the University",
+           "All of the above are true",
+       ],
+       answer: "All of the above are true",
+       feedback: "1963 was a great year of progress for the University. In addition to these amazing changes, the University also removed the mandatory service in the Corps of Cadets for all students. The creation of the Corps of Cadets was a requirement of the Morrill Act.",
+   },
+
+   {
+       question: "Which of the following are true with respect to the school mascot, Reveille?",
+       answers: [
+           "Miss Rev is the highest ranking member of the Corps of Cadets.",
+           "Tradition states that if Miss Rev barks during class, the class is dismissed.",
+           "When Miss Rev passes away, she is given full military honors and is buried on the North end of Kyle Field.",
+           "All of the above are true",
+       ],
+       answer: "All of the above are true",
+       feedback: "The story goes that in 1931, a group of cadets were driving back to town when they hit a dog. Although dogs were not allowed at the University housing, they snuck her in to take care of her. Unfortunately, when Reveille was played the next morning the injured dog began to bark and gave herself away, earning her the name Reveille.",
+   },
+
+   {
+       question: "What Aggie tradition takes place on April 21st?",
+       answers: [
+           "Midnight Yell",
+           "Fish Camp",
+           "Muster",
+           "Ring Dance",
+       ],
+       answer: "Muster",
+       feedback: "Muster, the 'Roll Call for the Absent', is a time honored tradition in which any Aggie student or former student that has passed away within that year will be honored. The ceremony consists of each name being called and answered 'Here' by those in attendance. This tradition is held worldwide, where ever two or more Aggies can gather together.",
+   },
+
+   {
+       question: "In 1999, what accident took place that cost the lives of 12 students?",
+       answers: [
+           "Bonfire",
+           "Thanksgiving Celebration",
+           "TAMU versus T.U. football game",
+           "Taps",
+       ],
+       answer: "Bonfire",
+       feedback: "Aggie Bonfire is a tradition established in 1907 that took place before every TAMU v. T.U. football game. The massive bonfire has always been a symbol for the Aggies 'burning desire' to beat the Longhorns. This tradition has been held every year with the exception of 2: 1963- the Bonfire was built but taken down after JFK's assassination, and 1999- the collapse of the bonfire resulted in 12 dead and 27 injured.",
+   },
+
+   {
+       question: "Getting your class ring at Texas A&#38;M is a big deal, what's with all the fuss?",
+       answers: [
+           "What fuss? It's just a class ring.",
+           "Earning your class ring means that you're automatically going to graduate. Congrats!",
+           "Who cares about the ring. The students just want to chug their pitcher of beer!",
+           "A student earning their ring shows their dedication: to their studies, to their University, and to their betterment. Plus, that Aggie Network...",
+       ],
+       answer: "A student earning their ring shows their dedication: to their studies, to their University, and to their betterment. Plus, that Aggie Network...",
+       feedback: "That piece of Aggie Gold is a BIG deal for students. There are a lot of traditions surrounding the ring (some official, some not so official): the Ring Ceremony, Ring Dance, and the Ring Dunk(here's that pitcher of beer). The Aggie Ring is also unique in that every ring is the same making it very recognizable to other students and former students!",
+   },
+
+   {
+       question: "What is unique about Texas A&#38;M's cheerleaders?",
+       answers: [
+           "To join the cheerleading squad you must be a member of the Corps of Cadets.",
+           "You can only cheer after being enrolled at the University for a year.",
+           "No cheerleaders here. Just 5 guys elected to be Yell Leaders!",
+           "Following Aggie Tradition, from when the school was all male, our cheerleading squad is composed of men from the Corps of Cadets.",
+       ],
+       answer: "No cheerleaders here. Just 5 guys elected to be Yell Leaders!",
+       feedback: "The Yell Leaders are comprised of 5 upperclass male students that lead all Aggies in Yells. (We don/'t cheer or boo, we hiss and yell!)",
+   },
+
+   {
+       question: "What is the offical greeting at Texas A&#38;M?",
+       answers: [
+           "Hiya There!",
+           "Hello!",
+           "What's up, Aggie?",
+           "Howdy!",
+       ],
+       answer: "Howdy!",
+       feedback: "Howdy is the official greeting at Texas A&#38;M! Students say it in passing, faculty use it to gain the attention of students or coworkers, and former students use it to find other former students outside of the University.",
+   },
+
+   {
+       question: "Why do Aggies say Whoop ?",
+       answers: [
+           "It's used to express excitement or approval.",
+           "It's used to end Yell's.",
+           "Whooping denotes a students status. Only upperclassmen are allowed to Whoop!",
+           "All of the above.",
+       ],
+       answer: "All of the above.",
+       feedback: "This is another great Aggie Tradition! Whooping is reserved for upperclassmen (and anyone footing the bill for school), and is heard frequently around campus. There are a lot of reasons to Whoop, but non-students hear it most frequently at our sporting events whenever we end a Yell or our War Hymn (fight song)!",
+   },
+
+];
